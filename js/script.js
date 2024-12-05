7b8c37a712b5c7ce0e038a106c8b5197927e3c29
zeno-citiulo <zenocit02@gmail.com>
2024-12-05 15:48:42 +0100

    feat: Mileston 2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 //https://jsonplaceholder.typicode.com/photos?_limit=6
+
+// 1-come prima cosa faccio la chiamata all'endpoint di JSON e mi viene restituito un array di oggetti ed ogniugno di loro rappresenta una fotografia.
+// 2-Potrei usare questi dati in modo che ogni card che io ho all'interno del mio progetto venga riempita con i dati che io ho all'interno del mio array di oggetti.
+
+
+
+
