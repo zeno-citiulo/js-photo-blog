1044287e9189f85a0fff407f6da18078f3a1fb97
zeno-citiulo <zenocit02@gmail.com>
2024-12-06 16:01:10 +0100

    feat:milestone 3 work in progress

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@
 // 1-come prima cosa faccio la chiamata all'endpoint di JSON e mi viene restituito un array di oggetti ed ogniugno di loro rappresenta una fotografia.
 // 2-Potrei usare questi dati in modo che ogni card che io ho all'interno del mio progetto venga riempita con i dati che io ho all'interno del mio array di oggetti.
 
+const endpoint = 'https://jsonplaceholder.typicode.com/photos?_limit=6'
+const image = document.querySelector('.place-holder')
+
+photoHolder()
+
+function photoHolder(){
+  axios.get(endpoint)
+    .then(res => {
+      //console.log(res.data);
+      response.data.forEach(photoHolder => printCard(photoHolder))
+    })
+    .catch(e => {
+      console.log(e);
+    })
+}
 
 
 
+function printCard(photoHolder){
+
+  console.log(photoHolder);
+  
+}
